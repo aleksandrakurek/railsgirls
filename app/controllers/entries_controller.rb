a1ca2900863da1abdebe78d58be73985e9b77988
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2018-06-30 17:48:09 +0200

    edit, delete, info

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
   def index
-    @entries = Entry.all
+    @entries = Entry.all.order(date: :desc)
   end
 
   def new
@@ -8,10 +8,8 @@ class EntriesController < ApplicationController
   end
 
   def create
-
-
     par = params.require(:entry)
-    @entry =  Entry.new(par.permit(:title, :contents))
+    @entry = Entry.new(par.permit(:title, :contents))
     @entry.date = Date.today
 
     if @entry.save
@@ -21,5 +19,30 @@ class EntriesController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @entry = Entry.find(params[:id])
+    par = params.require(:entry)
+    if @entry.update(par.permit(:title, :contents))
+      redirect_to action: :index
+    else
+      render :edit
+    end
+
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id]).destroy
+
+    flash[:info] = "Entry was destroyed"
+
+    redirect_to action: :index
+  end
+
 
 end
